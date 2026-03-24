2bc65064268a095df1c3723336c3fc62460ce06a
Tisha Choksi <tishachoksi18@gmail.com>
2026-03-24 16:46:45 +0530

    edit

--- backend/services/chunker.py
+++ backend/services/chunker.py
@@ -1,6 +1,6 @@
 # backend/services/chunker.py
 import fitz  # PyMuPDF
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def extract_text_from_pdf(file_bytes: bytes) -> str:
     doc = fitz.open(stream=file_bytes, filetype="pdf")
